ebf99fe69251f94729f995f92bab6f694ac48c6f
vatimetou ebeker <fatimaabcar2@gmail.com>
2022-01-04 15:18:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 // Write your code here
 
-function multiply(answer){
-    answer= num1* num2;
-    return 62;
+ const num1= 31
+ const num2=2
+     
+let multiply= num1* num2;
 
-}
+
+const random =6
+const num3=4
+const num4=40
+const mod = (num3%num4);
+
+const max = ( 2,3,5,20)
+    
